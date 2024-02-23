c92f1c46ebbc12e887624f9b4c49c54864f365aa
OngBen <104296848+OngBen@users.noreply.github.com>
2024-02-23 21:25:19 +0800

    Can now process GIFs

--- image_enhancer_process.py
+++ image_enhancer_process.py
@@ -4,15 +4,15 @@ from multiprocessing import Process, Queue
 import glob
 import os
 import time
-from PIL import Image,ImageEnhance
+from PIL import Image, ImageEnhance
 from multiprocessing import set_start_method
 
 Image.MAX_IMAGE_PIXELS = None
- 
+
 class Enhancement(BaseModel):
-        brightness_factor: int = 1
-        contrast_factor: int = 1
-        sharpness_factor: int = 1
+    brightness_factor: int = 1
+    contrast_factor: int = 1
+    sharpness_factor: int = 1
 
 class BulkImageEnhancer():
 
@@ -28,35 +28,56 @@ class BulkImageEnhancer():
         self.image_buffer = Queue()
         self.image_count = 0
         self.get_image_dir()
-  
 
     def createFolderIfNotExist(self):
         if not os.path.exists(self.output_dir):
             os.makedirs(self.output_dir)
 
     def get_image_dir(self):
-        png_image_list = glob.glob(os.path.join(self.input_dir,'*.png'))
-        jpg_image_list = glob.glob(os.path.join(self.input_dir,'*.jpg'))
-        gif_image_list = glob.glob(os.path.join(self.input_dir,'*.gif'))
+        png_image_list = glob.glob(os.path.join(self.input_dir, '*.png'))
+        jpg_image_list = glob.glob(os.path.join(self.input_dir, '*.jpg'))
+        gif_image_list = glob.glob(os.path.join(self.input_dir, '*.gif'))
         image_list = [*png_image_list, *jpg_image_list, *gif_image_list]
         self.image_count = len(image_list)
-        list(map(self.image_buffer.put,image_list))
-        
+        list(map(self.image_buffer.put, image_list))
 
-    def enhance_image(self, original_image):
+    def enhance_gif(self, original_image):
+        enhanced_frames = []
+        try:
+            while True:
+                current_frame = original_image.copy()
+                if current_frame.mode == "P":
+                    current_frame = current_frame.convert("RGBA")
+                enhanced_frame = self.apply_enhancements(current_frame)
+                enhanced_frames.append(enhanced_frame)
+                original_image.seek(original_image.tell() + 1)
+        except EOFError:
+            pass  
+        gif_name = os.path.basename(original_image.filename)
+        gif_path = os.path.join(self.output_dir, f"enhanced_{gif_name}")
+        enhanced_frames[0].save(gif_path, save_all=True, append_images=enhanced_frames[1:], loop=0)
+
+    def enhance_image(self, original_image, output_image):
+        enhanced_image = self.apply_enhancements(original_image)
+        enhanced_image.save(output_image)
+
+    def apply_enhancements(self, image):
+        # Apply enhancements to the image
         enhancement = self.enhancement_factors
-        brightness_enhancer = ImageEnhance.Brightness(original_image)
-        brightness_image = brightness_enhancer.enhance(enhancement.brightness_factor) 
+        brightness_enhancer = ImageEnhance.Brightness(image)
+        brightness_image = brightness_enhancer.enhance(enhancement.brightness_factor)
         contrast_enhancer = ImageEnhance.Contrast(brightness_image)
-        contrast_image = contrast_enhancer.enhance(enhancement.contrast_factor) 
+        contrast_image = contrast_enhancer.enhance(enhancement.contrast_factor)
         sharpness_enhancer = ImageEnhance.Sharpness(contrast_image)
-        sharpen_image = sharpness_enhancer.enhance(enhancement.sharpness_factor) 
+        sharpen_image = sharpness_enhancer.enhance(enhancement.sharpness_factor)
         return sharpen_image
 
     def process_image(self, input_image: str, output_image: str):
         image = Image.open(input_image)
-        enhanced_image = self.enhance_image(image)
-        enhanced_image.save(output_image)
+        if image.format=='GIF':
+            self.enhance_gif(image)
+        else:
+            self.enhance_image(image, output_image)      
 
     def get_image_to_process(self, process_name):
         while True:
@@ -68,10 +89,9 @@ class BulkImageEnhancer():
             self.process_image(input_image, output_image)
             print(f"{process_name} is done processing image {image_name}")
         return True
-    
+
     def __repr__(self) -> str:
         return f"BulkImageEnhancer processed {self.image_count} images, saved in {self.output_dir}\n"
-
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -85,19 +105,16 @@ def parse_args():
     return args
 
 if __name__ == "__main__":
-    args =  parse_args()
+    args = parse_args()
     bulk_enhancer = BulkImageEnhancer(args)
-    enhancer_process = [Process(target = bulk_enhancer.get_image_to_process, args = (f'Process-{idx}',)) for idx in range(int(args.num_threads))]
+    enhancer_process = [Process(target=bulk_enhancer.get_image_to_process, args=(f'Process-{idx}',)) for idx in range(int(args.num_threads))]
     start_time = time.perf_counter()
     for process in enhancer_process:
         process.start()
-    for thread in enhancer_process:
+    for process in enhancer_process:
         process.join()
     finish_time = time.perf_counter()
-    with open(os.path.join(args.output_dir,"output.txt"), 'w') as f:
-        f.writelines([repr(bulk_enhancer), 
-                      "Program finished in {} seconds".format(finish_time-start_time)
+    with open(os.path.join(args.output_dir, "output.txt"), 'w') as f:
+        f.writelines([repr(bulk_enhancer),
+                      "Program finished in {} seconds".format(finish_time - start_time)
                       ])
-
-    
-    
